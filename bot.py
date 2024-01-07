8f379053a582e6898811120c8850e58981f8c810
Lily Groth <24684567+cmajorix@users.noreply.github.com>
2024-01-07 17:57:39 -0600

    added requests and began updating embed

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from discord.ext import tasks
 from dotenv import load_dotenv
 import os
 import requests
+import datetime
 
 load_dotenv()
 discord_token = os.getenv("DISCORD_TOKEN")
@@ -10,14 +11,33 @@ discord_token = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+class BookingClient(discord.Client):
+    def __init__(self, client):
+        self.client = client
+        self.client.booking_channel = 0
+
+client = BookingClient(intents=intents)
 
 @tasks.loop(minutes=5)
 async def update_nexus_info(embed, ggleap_token):
     AUTH_URL = 'https://api.ggleap.com/production/authorization/public-api/auth'
     BOOKINGS_URL = 'https://api.ggleap.com/beta/bookings/get-bookings'
+    ggleap_token = os.getenv("GGLEAP_TOKEN")
+    
+    #do all the ggleap requests
+    auth = requests.post(AUTH_URL, data={'AuthToken':ggleap_token}).json()
+    auth = auth['Jwt']
 
+    header = {"Content-Type": "application/json", "Authorization": auth}
 
+    bookings_payload = {'Date': datetime.date.today().isoformat()}
+    bookings = requests.get(BOOKINGS_URL, bookings_payload, headers=header).json()
+
+    #update embed
+    embed.add_field(name='Monday, 5-7PM', 
+                    value='PCs 6-10 (VALORANT)')
+    
+    #
 
     pass
 
@@ -26,10 +46,6 @@ async def on_ready():
     base_embed = discord.Embed(color=discord.Color.from_str("#4E2A84"), 
                       title='Nexus Gaming Lounge Reservations',
                       type='rich')
-    base_embed.add_field(name='Monday, 5-7PM', 
-                    value='PCs 6-10 (VALORANT)')
-    base_embed.add_field(name='Tuesday, 7-9PM', 
-                    value='PC 3 (Overwatch)')
     base_embed.set_image(url='https://www.northwestern.edu/norris/arts-recreation/game-room/nexus-grand-opening025.jpg')
     ggleap_token = os.getenv("GGLEAP_TOKEN")
     update_nexus_info.start(base_embed, ggleap_token)
